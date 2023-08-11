85295586396cef3c5be2a310d498c0a65ceba5b2
Donato Zollo <davinazarra@gmail.com>
2023-08-11 19:41:15 -0300

    Home actualizado

--- redux/store.js
+++ redux/store.js
@@ -1,8 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
-import productReducer from "./reducers/product";
 import productsReducer from "./reducers/productsReducer";
 import productRedu from "./reducers/product";
-import userReducer from "./reducers/user";
+import userReducer from "./reducers/userReducer";
 
 
 export const store = configureStore({
--- src/views/Home.js
+++ src/views/Home.js
@@ -1,19 +1,36 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { useNavigation } from "@react-navigation/native";
+import { useDispatch, useSelector } from 'react-redux';
+import productsActions from "../../redux/actions/productsActions";
 import AntDesign from "react-native-vector-icons/AntDesign";
-import React from 'react';
 import { ImageBackground, StyleSheet, Text, View, Image, TouchableOpacity, ScrollView, TextInput } from 'react-native';
 
-export default function HomeScreen(props) {
-
+export default function HomeScreen() {
+    const dispatch = useDispatch();
     const navigation = useNavigation();
     const [isLoggedIn, setIsLoggedIn] = useState(false);
     const [searchText, setSearchText] = useState("");
 
+    const read_products = useSelector((store) => store.products.products);
+    const moreViews = read_products?.filter((elemento) => elemento.Moreview === true);
+
+    const imagesPhones = moreViews.filter(item => item.type === "Phones" || item.type === "Tabs");
+    const currentPhones = imagesPhones.slice(currentSlideCarousel2, currentSlideCarousel2 + imagesPorSlide);
+    const imagesDesktop = moreViews.filter(item => item.type === "NOTEBOOK" || item.type === "DESKTOP");
+    const currentDesktop = imagesDesktop.slice(currentSlideCarousel3, currentSlideCarousel3 + imagesPorSlide);
+    const imagesGamers = moreViews.filter(item => item.type === "pc" || item.type === "Chair" || item.type === "Mouse");
+    const currentGamers = imagesGamers.slice(currentSlideCarousel4, currentSlideCarousel4 + imagesPorSlide);
+    const imagesAudio = moreViews.filter(item => item.type === "TV" || item.type === "SPEAKERS" || item.type === "HEADPHONES" || item.type === "MICROPHONE" || item.type === "CAMERAS");
+    const currentAudio = imagesAudio.slice(currentSlideCarousel5, currentSlideCarousel5 + imagesPorSlide);
+    const imagesElectro = moreViews.filter(item => item.type === "Fridge" || item.type === "Air" || item.type === "Kitchen" || item.type === "Blender" || item.type === "Laundry");
+    const currentElectro= imagesElectro.slice(currentSlideCarousel6, currentSlideCarousel6 + imagesPorSlide);
+    const imagesPorSlide = 5;
+
     useEffect(() => {
+        dispatch(productsActions.read_products());
         checkLoginStatus();
-    }, []);
+    }, [dispatch]);
 
     const checkLoginStatus = async () => {
         try {
@@ -39,18 +56,18 @@ export default function HomeScreen(props) {
     const navigateTechPage = () => {
         navigation.navigate('TechsPage');
     };
-    
+
     return (
         <ImageBackground source={require('../../assets/backgroundHome.jpg')} style={styles.imageBackground}>
             <View style={styles.container}>
                 <View style={styles.navbar}>
                     <View style={styles.nav1}>
-                        <TouchableOpacity onPress={() => props.navigation.openDrawer()}>
-                        <AntDesign name="bars" style={styles.menu} />
+                        <TouchableOpacity onPress={() => navigation.openDrawer()}>
+                            <AntDesign name="bars" style={styles.menu} />
                         </TouchableOpacity>
                         <AntDesign name="shoppingcart" style={styles.logo} />
                     </View>
-                    <TextInput style={styles.searchInput} placeholder="Search..."  value={searchText} onChangeText={handleSearchChange} />
+                    <TextInput style={styles.searchInput} placeholder="Search..." value={searchText} onChangeText={handleSearchChange} />
                 </View>
                 <View style={styles.buttonContainer}>
                     <TouchableOpacity onPress={navigateHomeAppliancePage} style={styles.button}>
@@ -62,6 +79,86 @@ export default function HomeScreen(props) {
                     <TouchableOpacity onPress={navigateTechPage} style={styles.button}>
                         <Image source={require('../../assets/techs.png')} style={styles.buttonIcon} />
                     </TouchableOpacity>
+                </View>
+                <View style={{ paddingVertical: 16, paddingHorizontal: 10, marginTop: 32 }}>
+                    <Text style={{ fontSize: 20, color: 'white'  }}>More views on Phones & tabs</Text>
+                    <ScrollView horizontal>
+                        <ScrollView horizontal style={{  }}>
+                            {currentPhones.map((item, index) => (
+                                <View key={item._id} style={styles.carouselItem}>
+                                    <Image source={{ uri: item.cover_photo[0] }} style={styles.carouselImage} />
+                                    <Text>{item.brand}</Text>
+                                    <Text style={{ paddingHorizontal: 10, textAlign: 'center' }}>{item.title}</Text>
+                                    <Text>USD$ {item.price}</Text>
+                                    <Text style={{ fontSize: 16, color: 'green' }}>Withdraw it NOW!</Text>
+                                </View>
+                            ))}
+                        </ScrollView>
+                    </ScrollView>
+                </View>
+                <View style={{ paddingVertical: 16, paddingHorizontal: 10, marginTop: 32 }}>
+                    <Text style={{ fontSize: 20, color: 'white'  }}>More views on Desktops & Notebooks</Text>
+                    <ScrollView horizontal>
+                        <ScrollView horizontal style={{  }}>
+                            {currentDesktop.map((item, index) => (
+                                <View key={item._id} style={styles.carouselItem}>
+                                    <Image source={{ uri: item.cover_photo[0] }} style={styles.carouselImage} />
+                                    <Text>{item.brand}</Text>
+                                    <Text style={{ paddingHorizontal: 10, textAlign: 'center' }}>{item.title}</Text>
+                                    <Text>USD$ {item.price}</Text>
+                                    <Text style={{ fontSize: 16, color: 'green' }}>Withdraw it NOW!</Text>
+                                </View>
+                            ))}
+                        </ScrollView>
+                    </ScrollView>
+                </View>
+                <View style={{ paddingVertical: 16, paddingHorizontal: 10, marginTop: 32 }}>
+                    <Text style={{ fontSize: 20, color: 'white'  }}>More views on Gamers</Text>
+                    <ScrollView horizontal>
+                        <ScrollView horizontal style={{  }}>
+                            {currentGamers.map((item, index) => (
+                                <View key={item._id} style={styles.carouselItem}>
+                                    <Image source={{ uri: item.cover_photo[0] }} style={styles.carouselImage} />
+                                    <Text>{item.brand}</Text>
+                                    <Text style={{ paddingHorizontal: 10, textAlign: 'center' }}>{item.title}</Text>
+                                    <Text>USD$ {item.price}</Text>
+                                    <Text style={{ fontSize: 16, color: 'green' }}>Withdraw it NOW!</Text>
+                                </View>
+                            ))}
+                        </ScrollView>
+                    </ScrollView>
+                </View>
+                <View style={{ paddingVertical: 16, paddingHorizontal: 10, marginTop: 32 }}>
+                    <Text style={{ fontSize: 20, color: 'white'  }}>More views on Audio & Video</Text>
+                    <ScrollView horizontal>
+                        <ScrollView horizontal style={{  }}>
+                            {currentAudio.map((item, index) => (
+                                <View key={item._id} style={styles.carouselItem}>
+                                    <Image source={{ uri: item.cover_photo[0] }} style={styles.carouselImage} />
+                                    <Text>{item.brand}</Text>
+                                    <Text style={{ paddingHorizontal: 10, textAlign: 'center' }}>{item.title}</Text>
+                                    <Text>USD$ {item.price}</Text>
+                                    <Text style={{ fontSize: 16, color: 'green' }}>Withdraw it NOW!</Text>
+                                </View>
+                            ))}
+                        </ScrollView>
+                    </ScrollView>
+                </View>
+                <View style={{ paddingVertical: 16, paddingHorizontal: 10, marginTop: 32 }}>
+                    <Text style={{ fontSize: 20, color: 'white'  }}>More views on Appliances</Text>
+                    <ScrollView horizontal>
+                        <ScrollView horizontal style={{  }}>
+                            {currentElectro.map((item, index) => (
+                                <View key={item._id} style={styles.carouselItem}>
+                                    <Image source={{ uri: item.cover_photo[0] }} style={styles.carouselImage} />
+                                    <Text>{item.brand}</Text>
+                                    <Text style={{ paddingHorizontal: 10, textAlign: 'center' }}>{item.title}</Text>
+                                    <Text>USD$ {item.price}</Text>
+                                    <Text style={{ fontSize: 16, color: 'green' }}>Withdraw it NOW!</Text>
+                                </View>
+                            ))}
+                        </ScrollView>
+                    </ScrollView>
                 </View>
             </View>
         </ImageBackground>
@@ -118,6 +215,25 @@ const styles = StyleSheet.create({
     buttonIcon: {
         height: 40,
         width: 40,
+        resizeMode: 'contain',
+    },
+    carouselButton: {
+        padding: 10,
+    },
+    carouselItem: {
+        justifyContent: 'justify-center',
+        alignItems: 'center',
+        backgroundColor: 'white',
+        height: 300,
+        width: 200,
+        borderRadius: 10,
+        padding: 16,
+        alignItems: 'center',
+        margin: 10,
+    },
+    carouselImage: {
+        width: 150,
+        height: 150,
         resizeMode: 'contain',
     },
 });
