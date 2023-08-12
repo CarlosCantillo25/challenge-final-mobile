ecd1b17e7d317647e6c8222505e9430f5dcff3ef
Donato Zollo <davinazarra@gmail.com>
2023-08-12 14:23:52 -0300

    arreglo del arreglo

--- src/views/Home.js
+++ src/views/Home.js
@@ -15,17 +15,18 @@ export default function HomeScreen() {
     const read_products = useSelector((store) => store.products.products);
     const moreViews = read_products?.filter((elemento) => elemento.Moreview === true);
 
-    const imagesPhones = moreViews.filter(item => item.type === "Phones" || item.type === "Tabs");
-    const currentPhones = imagesPhones.slice(currentSlideCarousel2, currentSlideCarousel2 + imagesPorSlide);
-    const imagesDesktop = moreViews.filter(item => item.type === "NOTEBOOK" || item.type === "DESKTOP");
-    const currentDesktop = imagesDesktop.slice(currentSlideCarousel3, currentSlideCarousel3 + imagesPorSlide);
-    const imagesGamers = moreViews.filter(item => item.type === "pc" || item.type === "Chair" || item.type === "Mouse");
-    const currentGamers = imagesGamers.slice(currentSlideCarousel4, currentSlideCarousel4 + imagesPorSlide);
-    const imagesAudio = moreViews.filter(item => item.type === "TV" || item.type === "SPEAKERS" || item.type === "HEADPHONES" || item.type === "MICROPHONE" || item.type === "CAMERAS");
-    const currentAudio = imagesAudio.slice(currentSlideCarousel5, currentSlideCarousel5 + imagesPorSlide);
-    const imagesElectro = moreViews.filter(item => item.type === "Fridge" || item.type === "Air" || item.type === "Kitchen" || item.type === "Blender" || item.type === "Laundry");
-    const currentElectro= imagesElectro.slice(currentSlideCarousel6, currentSlideCarousel6 + imagesPorSlide);
     const imagesPorSlide = 5;
+
+    const imagesPhones = moreViews.filter(item => item.type === "Phones" || item.type === "Tabs");
+    const currentPhones = imagesPhones.slice(imagesPorSlide);
+    const imagesDesktop = moreViews.filter(item => item.type === "NOTEBOOK" || item.type === "DESKTOP");
+    const currentDesktop = imagesDesktop.slice(imagesPorSlide);
+    const imagesGamers = moreViews.filter(item => item.type === "pc" || item.type === "Chair" || item.type === "Mouse");
+    const currentGamers = imagesGamers.slice(imagesPorSlide);
+    const imagesAudio = moreViews.filter(item => item.type === "TV" || item.type === "SPEAKERS" || item.type === "HEADPHONES" || item.type === "MICROPHONE" || item.type === "CAMERAS");
+    const currentAudio = imagesAudio.slice(imagesPorSlide);
+    const imagesElectro = moreViews.filter(item => item.type === "Fridge" || item.type === "Air" || item.type === "Kitchen" || item.type === "Blender" || item.type === "Laundry");
+    const currentElectro= imagesElectro.slice(imagesPorSlide);
 
     useEffect(() => {
         dispatch(productsActions.read_products());
@@ -59,108 +60,110 @@ export default function HomeScreen() {
 
     return (
         <ImageBackground source={require('../../assets/backgroundHome.jpg')} style={styles.imageBackground}>
-            <View style={styles.container}>
-                <View style={styles.navbar}>
-                    <View style={styles.nav1}>
-                        <TouchableOpacity onPress={() => navigation.openDrawer()}>
-                            <AntDesign name="bars" style={styles.menu} />
-                        </TouchableOpacity>
-                        <AntDesign name="shoppingcart" style={styles.logo} />
+            <ScrollView>
+                <View style={styles.container}>
+                    <View style={styles.navbar}>
+                        <View style={styles.nav1}>
+                            <TouchableOpacity onPress={() => navigation.openDrawer()}>
+                                <AntDesign name="bars" style={styles.menu} />
+                            </TouchableOpacity>
+                            <AntDesign name="shoppingcart" style={styles.logo} />
+                        </View>
+                        <TextInput style={styles.searchInput} placeholder="Search..." value={searchText} onChangeText={handleSearchChange} />
                     </View>
-                    <TextInput style={styles.searchInput} placeholder="Search..." value={searchText} onChangeText={handleSearchChange} />
-                </View>
-                <View style={styles.buttonContainer}>
-                    <TouchableOpacity onPress={navigateHomeAppliancePage} style={styles.button}>
-                        <Image source={require('../../assets/iconAppliances.png')} style={styles.buttonIcon} />
-                    </TouchableOpacity>
-                    <TouchableOpacity onPress={navigateGamersPage} style={styles.button}>
-                        <Image source={require('../../assets/iconGamer.png')} style={styles.buttonIcon} />
-                    </TouchableOpacity>
-                    <TouchableOpacity onPress={navigateTechPage} style={styles.button}>
-                        <Image source={require('../../assets/techs.png')} style={styles.buttonIcon} />
-                    </TouchableOpacity>
-                </View>
-                <View style={{ paddingVertical: 16, paddingHorizontal: 10, marginTop: 32 }}>
-                    <Text style={{ fontSize: 20, color: 'white'  }}>More views on Phones & tabs</Text>
-                    <ScrollView horizontal>
-                        <ScrollView horizontal style={{  }}>
-                            {currentPhones.map((item, index) => (
-                                <View key={item._id} style={styles.carouselItem}>
-                                    <Image source={{ uri: item.cover_photo[0] }} style={styles.carouselImage} />
-                                    <Text>{item.brand}</Text>
-                                    <Text style={{ paddingHorizontal: 10, textAlign: 'center' }}>{item.title}</Text>
-                                    <Text>USD$ {item.price}</Text>
-                                    <Text style={{ fontSize: 16, color: 'green' }}>Withdraw it NOW!</Text>
-                                </View>
-                            ))}
+                    <View style={styles.buttonContainer}>
+                        <TouchableOpacity onPress={navigateHomeAppliancePage} style={styles.button}>
+                            <Image source={require('../../assets/iconAppliances.png')} style={styles.buttonIcon} />
+                        </TouchableOpacity>
+                        <TouchableOpacity onPress={navigateGamersPage} style={styles.button}>
+                            <Image source={require('../../assets/iconGamer.png')} style={styles.buttonIcon} />
+                        </TouchableOpacity>
+                        <TouchableOpacity onPress={navigateTechPage} style={styles.button}>
+                            <Image source={require('../../assets/techs.png')} style={styles.buttonIcon} />
+                        </TouchableOpacity>
+                    </View>
+                    <View style={{ paddingVertical: 16, paddingHorizontal: 10, marginTop: 32 }}>
+                        <Text style={{ fontSize: 20, color: 'white'  }}>More views on Phones & tabs</Text>
+                        <ScrollView horizontal>
+                            <ScrollView horizontal style={{  }}>
+                                {currentPhones.map((item, index) => (
+                                    <View key={item._id} style={styles.carouselItem}>
+                                        <Image source={{ uri: item.cover_photo[0] }} style={styles.carouselImage} />
+                                        <Text>{item.brand}</Text>
+                                        <Text style={{ paddingHorizontal: 10, textAlign: 'center' }}>{item.title}</Text>
+                                        <Text>USD$ {item.price}</Text>
+                                        <Text style={{ fontSize: 16, color: 'green' }}>Withdraw it NOW!</Text>
+                                    </View>
+                                ))}
+                            </ScrollView>
                         </ScrollView>
-                    </ScrollView>
-                </View>
-                <View style={{ paddingVertical: 16, paddingHorizontal: 10, marginTop: 32 }}>
-                    <Text style={{ fontSize: 20, color: 'white'  }}>More views on Desktops & Notebooks</Text>
-                    <ScrollView horizontal>
-                        <ScrollView horizontal style={{  }}>
-                            {currentDesktop.map((item, index) => (
-                                <View key={item._id} style={styles.carouselItem}>
-                                    <Image source={{ uri: item.cover_photo[0] }} style={styles.carouselImage} />
-                                    <Text>{item.brand}</Text>
-                                    <Text style={{ paddingHorizontal: 10, textAlign: 'center' }}>{item.title}</Text>
-                                    <Text>USD$ {item.price}</Text>
-                                    <Text style={{ fontSize: 16, color: 'green' }}>Withdraw it NOW!</Text>
-                                </View>
-                            ))}
+                    </View>
+                    <View style={{ paddingVertical: 16, paddingHorizontal: 10, marginTop: 32 }}>
+                        <Text style={{ fontSize: 20, color: 'white'  }}>More views on Desktops & Notebooks</Text>
+                        <ScrollView horizontal>
+                            <ScrollView horizontal style={{  }}>
+                                {currentDesktop.map((item, index) => (
+                                    <View key={item._id} style={styles.carouselItem}>
+                                        <Image source={{ uri: item.cover_photo[0] }} style={styles.carouselImage} />
+                                        <Text>{item.brand}</Text>
+                                        <Text style={{ paddingHorizontal: 10, textAlign: 'center' }}>{item.title}</Text>
+                                        <Text>USD$ {item.price}</Text>
+                                        <Text style={{ fontSize: 16, color: 'green' }}>Withdraw it NOW!</Text>
+                                    </View>
+                                ))}
+                            </ScrollView>
                         </ScrollView>
-                    </ScrollView>
-                </View>
-                <View style={{ paddingVertical: 16, paddingHorizontal: 10, marginTop: 32 }}>
-                    <Text style={{ fontSize: 20, color: 'white'  }}>More views on Gamers</Text>
-                    <ScrollView horizontal>
-                        <ScrollView horizontal style={{  }}>
-                            {currentGamers.map((item, index) => (
-                                <View key={item._id} style={styles.carouselItem}>
-                                    <Image source={{ uri: item.cover_photo[0] }} style={styles.carouselImage} />
-                                    <Text>{item.brand}</Text>
-                                    <Text style={{ paddingHorizontal: 10, textAlign: 'center' }}>{item.title}</Text>
-                                    <Text>USD$ {item.price}</Text>
-                                    <Text style={{ fontSize: 16, color: 'green' }}>Withdraw it NOW!</Text>
-                                </View>
-                            ))}
+                    </View>
+                    <View style={{ paddingVertical: 16, paddingHorizontal: 10, marginTop: 32 }}>
+                        <Text style={{ fontSize: 20, color: 'white'  }}>More views on Gamers</Text>
+                        <ScrollView horizontal>
+                            <ScrollView horizontal style={{  }}>
+                                {currentGamers.map((item, index) => (
+                                    <View key={item._id} style={styles.carouselItem}>
+                                        <Image source={{ uri: item.cover_photo[0] }} style={styles.carouselImage} />
+                                        <Text>{item.brand}</Text>
+                                        <Text style={{ paddingHorizontal: 10, textAlign: 'center' }}>{item.title}</Text>
+                                        <Text>USD$ {item.price}</Text>
+                                        <Text style={{ fontSize: 16, color: 'green' }}>Withdraw it NOW!</Text>
+                                    </View>
+                                ))}
+                            </ScrollView>
                         </ScrollView>
-                    </ScrollView>
-                </View>
-                <View style={{ paddingVertical: 16, paddingHorizontal: 10, marginTop: 32 }}>
-                    <Text style={{ fontSize: 20, color: 'white'  }}>More views on Audio & Video</Text>
-                    <ScrollView horizontal>
-                        <ScrollView horizontal style={{  }}>
-                            {currentAudio.map((item, index) => (
-                                <View key={item._id} style={styles.carouselItem}>
-                                    <Image source={{ uri: item.cover_photo[0] }} style={styles.carouselImage} />
-                                    <Text>{item.brand}</Text>
-                                    <Text style={{ paddingHorizontal: 10, textAlign: 'center' }}>{item.title}</Text>
-                                    <Text>USD$ {item.price}</Text>
-                                    <Text style={{ fontSize: 16, color: 'green' }}>Withdraw it NOW!</Text>
-                                </View>
-                            ))}
+                    </View>
+                    <View style={{ paddingVertical: 16, paddingHorizontal: 10, marginTop: 32 }}>
+                        <Text style={{ fontSize: 20, color: 'white'  }}>More views on Audio & Video</Text>
+                        <ScrollView horizontal>
+                            <ScrollView horizontal style={{  }}>
+                                {currentAudio.map((item, index) => (
+                                    <View key={item._id} style={styles.carouselItem}>
+                                        <Image source={{ uri: item.cover_photo[0] }} style={styles.carouselImage} />
+                                        <Text>{item.brand}</Text>
+                                        <Text style={{ paddingHorizontal: 10, textAlign: 'center' }}>{item.title}</Text>
+                                        <Text>USD$ {item.price}</Text>
+                                        <Text style={{ fontSize: 16, color: 'green' }}>Withdraw it NOW!</Text>
+                                    </View>
+                                ))}
+                            </ScrollView>
                         </ScrollView>
-                    </ScrollView>
-                </View>
-                <View style={{ paddingVertical: 16, paddingHorizontal: 10, marginTop: 32 }}>
-                    <Text style={{ fontSize: 20, color: 'white'  }}>More views on Appliances</Text>
-                    <ScrollView horizontal>
-                        <ScrollView horizontal style={{  }}>
-                            {currentElectro.map((item, index) => (
-                                <View key={item._id} style={styles.carouselItem}>
-                                    <Image source={{ uri: item.cover_photo[0] }} style={styles.carouselImage} />
-                                    <Text>{item.brand}</Text>
-                                    <Text style={{ paddingHorizontal: 10, textAlign: 'center' }}>{item.title}</Text>
-                                    <Text>USD$ {item.price}</Text>
-                                    <Text style={{ fontSize: 16, color: 'green' }}>Withdraw it NOW!</Text>
-                                </View>
-                            ))}
+                    </View>
+                    <View style={{ paddingVertical: 16, paddingHorizontal: 10, marginTop: 32 }}>
+                        <Text style={{ fontSize: 20, color: 'white'  }}>More views on Appliances</Text>
+                        <ScrollView horizontal>
+                            <ScrollView horizontal style={{  }}>
+                                {currentElectro.map((item, index) => (
+                                    <View key={item._id} style={styles.carouselItem}>
+                                        <Image source={{ uri: item.cover_photo[0] }} style={styles.carouselImage} />
+                                        <Text>{item.brand}</Text>
+                                        <Text style={{ paddingHorizontal: 10, textAlign: 'center' }}>{item.title}</Text>
+                                        <Text>USD$ {item.price}</Text>
+                                        <Text style={{ fontSize: 16, color: 'green' }}>Withdraw it NOW!</Text>
+                                    </View>
+                                ))}
+                            </ScrollView>
                         </ScrollView>
-                    </ScrollView>
+                    </View>
                 </View>
-            </View>
+            </ScrollView>
         </ImageBackground>
     );
 }
